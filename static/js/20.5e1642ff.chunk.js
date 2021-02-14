f123caddacb545909183c3ac258379a997d5d6f3
woodRock <woodRock@users.noreply.github.com>
2021-02-14 09:09:43 +0000

    Deploying to main from @ woodRock/silver-rotary-phone@501ae89d1df458955228edb03e07bac809fb647c 🚀

---
+++ static/js/20.5e1642ff.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptrack=this.webpackJsonptrack||[]).push([[20],{232:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return i}));var c=n(8),i=function(){function t(t){Object(c.k)(this,t)}return t.prototype.render=function(){return Object(c.i)(c.b,null,Object(c.i)("slot",null))},t}();i.style=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=20.5e1642ff.chunk.js.map
